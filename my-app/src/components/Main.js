d8aceaeba2ef41498a0880303d085f37aa75a0f6
sam-phillips21 <phillips.sam@gmail.com>
2022-12-07 15:23:52 -0700

    basic bio and links

--- my-app/src/components/Main.js
+++ my-app/src/components/Main.js
@@ -1,30 +1,54 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-//main to include 
+import { Image, Container, Card } from "react-bootstrap"
+
+//main to include most details.
+
 const Main = () => {
+
     return(
+
         <>
-        <div className="home">
 
-			<h1 className="page-heading">Welcome to my Portfolio!</h1>
-            <p>
+        <h1 className="page-heading">Welcome to my Portfolio!</h1>
+        <p className='welcomeBio'>
+            Hi! Welcome, I am Sam and I am a software engineer! I am new to the world of software and ready to learn everything I can about the industry. I have a lot of experience in project management and building things. I decided I wanted to learn to build things in the digital world.
+        </p>
+        <p className= 'projectLinks'>
+            <a href= "https://wyotrails.fly.dev/" target='_blank'>My first application</a> 
+            <br />
+            <a href= "https://thegoodavocado.netlify.app/">First Group Project</a>
+            <a href= "https://github.com/sam-phillips21"></a>
 
-                I have lots of work to do on mi portfolio! 
+        </p>
+        <div className='mx-4'>
+            <Card.Body>
 
+                <div className="projImages">
+
+                    <Image src= "https://i.imgur.com/XhDxiYe.png" alt="ps" style={{width: "600px" }} />
                 
+                    <Image src="https://i.imgur.com/i2Scm9P.jpg" alt="gva"style={{width: "600px" }}/>
 
-            </p>
+                    <Image src="https://i.imgur.com/FwlaMgZ.png" alt="wyo"style={{width: "600px" }}/>
 
-            <p>
+                </div>
 
-           
-            </p>
-            <p>
+            </Card.Body>
 
-            <Link to='https://www.linkedin.com/in/samphillips100/' className='btn btn-outline-info mx-1'></Link>
 
-            </p>
-        </div>
+                <Card.Body>
+            
+                    <img
+                        style={{ width: 600 }}
+                        // src={home}
+                        alt={""}
+                    />
+                 </Card.Body>
+       
+                <Link to='./linkedin.com/in/samphillips100/' className='btn btn-outline-info mx-1'></Link>
+
+       </div>
         </>
     )
 }
