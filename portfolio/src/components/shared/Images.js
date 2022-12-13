2fbe87a70126cde96f5312ab65746f8f756fbc25
sam-phillips21 <phillips.sam@gmail.com>
2022-12-12 22:00:02 -0700

    restructure

--- portfolio/src/components/shared/Images.js
+++
@@ -1,7 +0,0 @@
-const Images = {
-    sp: "https://i.imgur.com/NNVj5I3.jpg",
-    sp2: "https://i.imgur.com/H1eWv6Z.jpg",
-    sp3:"https://i.imgur.com/CMkVM1o.jpg"
-}
-
-module.exports = Images
--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import { Link } from 'react-router-dom'
-// import { Container } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
 const linkStyle = {
 	color: 'Black',
 	textDecoration: 'none'
